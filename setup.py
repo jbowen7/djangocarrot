0194ef51d677fbc01e03b3467da2e399570f68f1
John Bowen <jbowen7@gmail.com>
2017-11-16 00:39:34 -0800

    typo

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
   name = 'djangocarrot',
-  packages = ['djangocarrot'], 
+  packages = ['djangocarrot'],
   version = '1.1',
-  description = 'Carrot -- A simple task Queue for Django.'
+  description = 'Carrot -- A simple task Queue for Django.',
   author = 'John Bowen',
   author_email = 'jbowen7@gmail.com',
   url = 'https://github.com/jbowen7/djangocarrot',
-  keywords = ['django', 'task', 'queue'], 
+  keywords = ['django', 'task', 'queue'],
   classifiers = [],
 )
