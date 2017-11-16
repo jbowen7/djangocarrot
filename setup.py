8cfb336331446118e0a6b641d65a64650ce51444
John Bowen <jbowen7@gmail.com>
2017-11-16 00:37:21 -0800

    restructure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'djangocarrot',
+  packages = ['djangocarrot'], 
+  version = '1.1',
+  description = 'Carrot -- A simple task Queue for Django.'
+  author = 'John Bowen',
+  author_email = 'jbowen7@gmail.com',
+  url = 'https://github.com/jbowen7/djangocarrot',
+  keywords = ['django', 'task', 'queue'], 
+  classifiers = [],
+)
