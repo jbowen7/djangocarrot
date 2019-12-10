5105dc0bf857ff4da3d3a95d15fcf44ea9baac4f
John Bowen <jbowen7@gmail.com>
2019-12-10 20:35:07 +0000

    restructured project, module relocated to src directory. Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python3
+
+import os
 from setuptools import setup, find_packages
+
+BASE_DIR = os.path.dirname(__file__)
+VERSION = '1.1.1'
+
 setup(
   name = 'djangocarrot',
-  packages = find_packages(),
-  version = '1.1.1',
+  version = VERSION,
   description = 'Carrot -- A simple task Queue for Django.',
   author = 'John Bowen',
   author_email = 'jbowen7@gmail.com',
   url = 'https://github.com/jbowen7/djangocarrot',
   keywords = ['django', 'task', 'queue'],
-  classifiers = [],
   install_requires = ['pika',],
+  package_dir={'': 'src'},
+  packages=find_packages(where='src', exclude=[]),
 )
